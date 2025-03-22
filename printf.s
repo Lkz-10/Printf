dfe3c1c4e8a3842db8f3e9448bf103aedd5eccee
Georgiy Kazarin <kazarin.gm@phystech.edu>
2025-03-22 20:14:23 +0300

    %b, %o and %x handlers added (universal number handler)

--- printf.s
+++ printf.s
@@ -10,12 +10,19 @@ _start:
 
     mov rsi, String             ; string to print
     mov rdi, Buffer
-    push 100
-    push 'T'
-    push 'I'
-    push 'N'
-    push 'E'                    ;
-    push 'Z'                    ; arguments for %c
+
+; arguments:
+    ; push 100
+    ; push 100
+    ; push 'T'
+    ; push 'I'
+    ; push 'N'
+    ; push 'E'
+    ; push 'Z'
+    push 10
+    push 10
+    push 10
+    push 10
 
 Symbol:
     cmp byte [rsi], '$'         ; terminal symbol
@@ -37,7 +44,10 @@ Prcnt_handler: ; DESTROYING RAX
     add rax, JumpTable          ;
     jmp [rax]                   ; jumping to [JumpTable + ASCII * 8]
 
+;========================================%b===========================================
 prcnt_b:
+    mov rcx, 2
+    jmp NumberHandler
 
 ;========================================%c===========================================
 prcnt_c: ; DESTROYING RAX
@@ -49,7 +59,12 @@ prcnt_c: ; DESTROYING RAX
     jmp Symbol                  ; consider next symbol
 
 ;========================================%d===========================================
-prcnt_d: ; DESTROYING RAX, RDX, RCX
+prcnt_d:
+    mov rcx, 10
+    jmp NumberHandler
+
+;===============================Universal Number Hadler===============================
+NumberHandler: ; DESTROYING RAX, RDX. INPUT: INDEX OF THE NUMBER SYSTEM
     pop rax                     ; getting the number
     push rsi                    ; saving rsi
     mov rsi, NumBuffer          ; buffer for the number
@@ -59,31 +74,40 @@ GetDigit:                       ; while (rax > 0)
     je PutDigit                 ;   NumBuffer[rsi - NumBuffer] = rax % 10;
                                 ;   rax /= 10;
     xor rdx, rdx                ;   rsi++;
-    mov rcx, 10                 ; }
-    div rcx                     ;
+    div rcx                     ; }
     mov [rsi], dl               ;
     inc rsi                     ;
     jmp GetDigit                ;
 
 PutDigit:                       ;
     cmp rsi, NumBuffer          ; while (rsi > NumBuffer)
-    je PrcntDEnd                ; {
+    je NumHandlerEnd            ; {
     dec rsi                     ;   rsi--;
     add byte [rsi], '0'         ;   NumBuffer[rsi - NumBuffer] += '0';
+    cmp byte [rsi], '9'         ;   if (NumBuffer[rsi - NumBuffer] <= '9')
+    jbe NotHex                  ;   { // 7 symbols between '9' and 'A' in ASCII table:
+    add byte [rsi], 7           ;       NumBuffer[rsi - NumBuffer] += 7;
+NotHex:                         ;   }
     movsb                       ;   Buffer[rdi - Buffer] = NumBuffer[rsi - NumBuffer];
     dec rsi                     ;   rdi++;
     jmp PutDigit                ; }
 
-PrcntDEnd:
+NumHandlerEnd:
     pop rsi
     inc rsi
     jmp Symbol                  ; considering next symbol
 
-;=====================================================================================
-
+;========================================%o===========================================
 prcnt_o:
+    mov rcx, 8
+    jmp NumberHandler
+
+;========================================%s===========================================
 prcnt_s:
+;========================================%x===========================================
 prcnt_x:
+    mov rcx, 16
+    jmp NumberHandler
 
 ;========================================%%===========================================
 prcnt_prcnt:
@@ -156,7 +180,8 @@ ErrMsgLen equ $ - ErrorMsg
 DbgMsg    db "BUGBUGBUGBUGBUGBUG", 0x0a
 NumBuffer dq 32 dup (0)
 DbgMsgLen equ $ - DbgMsg
-String    dq "The winner  is ... %c%c%c%c%c!", 0x0a, "Probability is ... %d%%", 0x0a, "$"
+; String    dq "The winner  is ... %c%c%c%c%c!", 0x0a, "Probability is ... %d%% of %d%%", 0x0a, "$"
+String    dq "Dec - %d, Hex - %x, Oct - %o, Bin - %b", 0x0a, "$"
 
 ;     push rsi
 ;     push rdi
